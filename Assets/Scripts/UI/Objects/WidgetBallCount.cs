cc69a72d72954cd612067c6d162ca3cf61d0317a
Thong Nguyen <nguyen.thong.1988@gmail.com>
2019-01-08 23:50:35 +0700

    +add

---
+++ Assets/Scripts/UI/Objects/WidgetBallCount.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WidgetBallCount : MonoBehaviour
+{
+    public Image BallImage;
+    public TextMeshProUGUI ScoreText;
+    public Ball.Color Color;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public Vector3 HitPointPosition
+    {
+        get
+        {
+            if (BallImage) return BallImage.rectTransform.position;
+
+            return transform.position;
+        }
+    }
+}
