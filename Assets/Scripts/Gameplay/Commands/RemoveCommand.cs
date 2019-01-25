4c6f2bb6c924c4175001e0912f7b83f66b701ebf
Thong Nguyen <nguyen.thong.1988@gmail.com>
2019-01-25 09:13:25 +0700

    +add command

---
+++ Assets/Scripts/Gameplay/Commands/RemoveCommand.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class RemoveCommand : ICommand
+{
+    public Vector2 Position;
+
+    private Board mBoard;
+
+    public RemoveCommand(Board board)
+    {
+        mBoard = board;
+    }
+
+    public RemoveCommand(Board board, Vector2 position) : this(board)
+    {
+        Position = position;
+    }
+
+    public void Execute()
+    {
+
+    }
+
+    public void Reverse()
+    {
+
+    }
+}
